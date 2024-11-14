cba7696af83976e2b6e32143078c37e6f30e6fb0
dvaled <davaalif0@gmail.com>
2024-11-14 10:51:33 +0700

    change assethistory structur nipp to nullable

--- Master_GCM/Models/TrnAssetHistoryModel.cs
+++ Master_GCM/Models/TrnAssetHistoryModel.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 public class TRNASSETHISTORYMODEL{
     public int IDASSETHISTORY { get; set; }
     public required string ASSETCODE { get; set; }
-    public int NIPP { get; set; }
+    public int? NIPP { get; set; }
     public required string PICADDED { get; set; }
     public DateOnly DATEADDED { get; set; }
 
