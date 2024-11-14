8b0bd6dc0ff2aa5eadbc3cb7fc65b0a89d438f1a
dvaled <davaalif0@gmail.com>
2024-11-14 10:49:30 +0700

    handling auto-increment idasseth, nipp, date,  employee and asset fk

---
+++ Master_GCM/Migrations/20241114033839_ChangeAssetHistoryNIPPtoNull.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Master_GCM.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeAssetHistoryNIPPtoNull : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "NIPP",
+                table: "TRN_HIST_ASSET",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "NIPP",
+                table: "TRN_HIST_ASSET",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+    }
+}
