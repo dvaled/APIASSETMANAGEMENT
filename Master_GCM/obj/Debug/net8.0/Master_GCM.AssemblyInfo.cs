e180a65b5894605ee2162dc78ae55a8e67e65830
dvaled <davaalif0@gmail.com>
2024-11-25 09:50:23 +0700

    app settings

--- Master_GCM/obj/Debug/net8.0/Master_GCM.AssemblyInfo.cs
+++ Master_GCM/obj/Debug/net8.0/Master_GCM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Master_GCM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f99cb5aab4e823796a6b9ec0f4985ca38d4401e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cff34fb3d2b048e8f24e62091a6c49a4c79e2044")]
 [assembly: System.Reflection.AssemblyProductAttribute("Master_GCM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Master_GCM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
