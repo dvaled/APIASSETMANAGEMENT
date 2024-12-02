7fd6058195a103605c094dbb3838a6ab86154a52
dvaled <davaalif0@gmail.com>
2024-12-02 15:42:17 +0700

    Up  PURCHASEDATE to the database

---
+++ Master_GCM/Migrations/20241202024729_AddPurchaseDate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Master_GCM.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPurchaseDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "PURCHASEDATE",
+                table: "TRN_ASSET",
+                type: "date",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PURCHASEDATE",
+                table: "TRN_ASSET");
+        }
+    }
+}
