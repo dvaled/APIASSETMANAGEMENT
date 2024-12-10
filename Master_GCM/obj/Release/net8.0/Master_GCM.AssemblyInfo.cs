0b1bb822aac57cb88e75918a87828fedb8f8c22f
dvaled <davaalif0@gmail.com>
2024-12-10 13:49:53 +0700

    Modified upload url

--- Master_GCM/obj/Release/net8.0/Master_GCM.AssemblyInfo.cs
+++ Master_GCM/obj/Release/net8.0/Master_GCM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Master_GCM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcff127c994982a72c59a27d4b4a93dcef3d878e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2357749199750464aed5c894f86308be162923f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Master_GCM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Master_GCM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
