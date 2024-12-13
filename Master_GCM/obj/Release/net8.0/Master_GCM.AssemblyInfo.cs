b90ac5781c49d400e37c2e6867d0f68e570b9f06
dvaled <davaalif0@gmail.com>
2024-12-13 09:14:18 +0700

    release v0.2

--- Master_GCM/obj/Release/net8.0/Master_GCM.AssemblyInfo.cs
+++ Master_GCM/obj/Release/net8.0/Master_GCM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Master_GCM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ed7288e115941ef4fcfd916b4fbf47f2b1997e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00c840daa40edc60f6078f4bffa441a453f9b5f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Master_GCM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Master_GCM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
